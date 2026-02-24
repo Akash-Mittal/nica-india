5049bc170599e3479cd9fa5eefa5bd6eafd1b5ed
mmt-akash-mittal <mittala@media-saturn.com>
2026-02-24 05:57:49 +0100

    refactor: Update template placeholder format
    - Removes curly braces from template placeholder keys in `build_email.php`.
    - This change aligns the replacement keys with the format used in the WhatsApp template file, ensuring correct substitution.

--- build_email.php
+++ build_email.php
@@ -134,9 +134,9 @@ $anniversaryRowsText = buildAnniversaryRows(is_array($anniversaries) ? $annivers
 $helplineRowsText = buildHelplineRows(is_array($helpByLocation) ? $helpByLocation : []);
 
 $replacements = [
-        'TITLE_DATE'       => wa_escape($titleDate),
-        'ANNIVERSARY_ROWS' => $anniversaryRowsText,
-        'HELPLINE_ROWS'    => $helplineRowsText,
+        '{{TITLE_DATE}}'       => wa_escape($titleDate),
+        '{{ANNIVERSARY_ROWS}}' => $anniversaryRowsText,
+        '{{HELPLINE_ROWS}}'    => $helplineRowsText,
 ];
 $whatsappText = strtr($templateText, $replacements);
 
